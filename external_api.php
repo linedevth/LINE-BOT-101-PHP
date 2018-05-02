655a9fe2d916097f4b64ee1547620fdd15f2f8fd
Jirawat Karanwittayakarn <jirawatee@gmail.com>
2018-05-02 15:26:18 +0700

    Hide some code for hands-on

--- external_api.php
+++ external_api.php
@@ -7,28 +7,26 @@ define('LINE_MESSAGING_API', 'https://api.line.me/v2/bot/message/');
 define('LINE_USER_ID', '<YOUR_USER_ID>');
 define('SANOOK_API', 'http://m.sanook.com/feed/weblog/info/exchange/?format=json');
 
-if (isset($_GET) && $_GET['push']) {
+if (isset($_GET['push'])) {
       push();
 } else {
       reply();
 }
 
 function push() {
-      $exchangeData = file_get_contents(SANOOK_API);
-      $exchangeArray = json_decode($exchangeData, true);
-      foreach ($exchangeArray['item'] as $item) {
-            $pushMsg .= $item['title']." ".$item['description']."\n\n";
-      }
-      $pushMsg = substr($pushMsg, 0, -2);
-      $message = [
-            'type' => 'text',
-            'text' => $pushMsg
-      ];
-      $body = [
-            'to' => LINE_USER_ID,
-            'messages' => [$message]
-      ];
-      curl('push', json_encode($body));
+      // get JSON from Sanook API
+      
+      // convert JSON to Array
+      
+      // foreach items
+      
+      
+      // Msg payload
+
+      // Body payload (replyToken => to)
+
+      // Curl
+      
       echo $pushMsg;
 }
 
@@ -45,29 +43,19 @@ function reply() {
             
                   // Reply only when message type is text
                   if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
-                        $text = strtoupper(trim($event['message']['text']));
+                        // get JSON from Sanook API
+      
+                        // convert JSON to Array
+
+                        // trim and upper
+
+                        // Switch currencies
+
+                        // Msg payload
                         
-                        $exchangeData = file_get_contents(SANOOK_API);
-                        $exchangeArray = json_decode($exchangeData, true);
+                        // Body payload
 
-                        switch ($text) {
-                              case 'USD': $replyMsg = $exchangeArray['item'][0]['description']; break;
-                              case 'JPY': $replyMsg = $exchangeArray['item'][1]['description']; break;
-                              case 'EUR': $replyMsg = $exchangeArray['item'][2]['description']; break;
-                              case 'GBP': $replyMsg = $exchangeArray['item'][3]['description']; break;
-                              case 'HKD': $replyMsg = $exchangeArray['item'][4]['description']; break;
-                              default: $replyMsg = 'Sorry "'.$text.'" is not supported.';
-                        }
-
-                        $message = [
-                              'type' => 'text',
-                              'text' => $replyMsg
-                        ];
-                        $body = [
-                              'replyToken' => $event['replyToken'],
-                              'messages' => [$message]
-                        ];
-                        curl('reply', json_encode($body));
+                        // Curl
                   }
             }
       }
